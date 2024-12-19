4ca67acd34563beb679e574c830fb3bc93556929
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2024-12-19 10:38:32 +0100

    Update AccesoSeguro.py

--- AccesoSeguro.py
+++ AccesoSeguro.py
@@ -6,18 +6,18 @@ def run_web():
     os.system("python3 web.py")
 
 # Función para ejecutar el reconocimiento facial
-def run_reconocer_usuarios():
-    os.system("python3 reconocer_usuarios.py")
+def run_caja():
+    os.system("python3 caja.py")
 
 if __name__ == "__main__":
     # Crear procesos
     web = multiprocessing.Process(target=run_web)
-    reconocer_usuarios = multiprocessing.Process(target=run_reconocer_usuarios)
+    reconocer_usuarios = multiprocessing.Process(target=run_caja)
 
     # Iniciar procesos
     web.start()
-    reconocer_usuarios.start()
+    caja.start()
 
     # Esperar a que terminen los procesos
     web.join()
-    reconocer_usuarios.join()
+    caja.join()

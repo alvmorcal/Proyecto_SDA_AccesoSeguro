6f5b9b606b23dfeb0fc9a5a788278ca472ac820d
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2025-01-12 21:31:13 +0100

    Update AccesoSeguro.py

--- AccesoSeguro.py
+++ AccesoSeguro.py
@@ -1,23 +1,35 @@
-import multiprocessing
-import os
+import multiprocessing  # Biblioteca para crear y manejar procesos independientes
+import os  # Biblioteca para interactuar con el sistema operativo
 
 # Función para ejecutar el servidor Flask
 def run_web():
+    # Usa un comando del sistema operativo para ejecutar el archivo "web.py"
     os.system("python3 web.py")
 
 # Función para ejecutar el reconocimiento facial
 def run_caja():
+    # Usa un comando del sistema operativo para ejecutar el archivo "caja.py"
     os.system("python3 caja.py")
 
 if __name__ == "__main__":
-    # Crear procesos
+    # Bloque principal del script, asegura que el código dentro de este bloque
+    # solo se ejecute cuando el archivo se ejecuta directamente, no cuando es importado.
+
+    # Crear un proceso independiente para el servidor web
     web = multiprocessing.Process(target=run_web)
+
+    # Crear un proceso independiente para el sistema de reconocimiento facial
     caja = multiprocessing.Process(target=run_caja)
 
-    # Iniciar procesos
+    # Iniciar el proceso que ejecuta "web.py"
     web.start()
+
+    # Iniciar el proceso que ejecuta "caja.py"
     caja.start()
 
-    # Esperar a que terminen los procesos
+    # Esperar a que el proceso "web" termine antes de continuar
     web.join()
+
+    # Esperar a que el proceso "caja" termine antes de continuar
     caja.join()
+
